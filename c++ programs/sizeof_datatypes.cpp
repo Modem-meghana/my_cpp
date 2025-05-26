35bcd0fd8f6d22481fe9393b0415573bf63cd7e0
Modem-meghana <142580363+Modem-meghana@users.noreply.github.com>
2025-05-26 22:55:29 +0530

    Add files via upload

--- c++ programs/sizeof_datatypes.cpp
+++ c++ programs/sizeof_datatypes.cpp
@@ -2,13 +2,8 @@
 using namespace std;
 int main()
 {
-    size_t a,d,c,f;
-    a = sizeof(int);
-    d = sizeof(double);
-    c = sizeof(char);
-    f = sizeof(float);
-    cout << "sizeof int  " << a << endl;
-    cout << "sizeof double  " << d << endl;
-    cout << "sizeof char  " << c << endl;
-    cout << "sizeof float  " << f << endl;
+    cout << "size of int  " << sizeof(int) << endl;
+    cout << "size of float  " << sizeof(float) << endl;
+    cout << "size of double  " << sizeof(double) << endl;
+    cout << "size of char  " << sizeof(char) << endl;
 }
